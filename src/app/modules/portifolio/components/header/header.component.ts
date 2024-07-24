fbb3b094b3915d2bb42f3398999dec2a90e9374b
PauloRicardoDev <pauloricardochagas.dev@gmail.com>
2024-07-24 16:33:50 -0300

    menu item adjustments

--- src/app/modules/portifolio/components/header/header.component.ts
+++ src/app/modules/portifolio/components/header/header.component.ts
@@ -1,28 +1,38 @@
 import { Component } from '@angular/core';
-import {Router} from "@angular/router";
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-header',
   standalone: true,
   imports: [],
   templateUrl: './header.component.html',
-  styleUrl: './header.component.scss'
+  styleUrls: ['./header.component.scss'] // Correção aqui
 })
 export class HeaderComponent {
+  isLoading: boolean = false;
 
   linkLinkedin() {
-    window.location.href = "https://www.linkedin.com/in/paulo-ricardo-chagas-99904b244?utm_source=share&utm_campaign=share_via&utm_content=profile&utm_medium=android_app";
+    window.open("https://www.linkedin.com/in/paulo-ricardo-chagas-99904b244?utm_source=share&utm_campaign=share_via&utm_content=profile&utm_medium=android_app", "_blank", "noopener,noreferrer");
   }
 
-  linkWhatsApp(){
-    window.location.href = "https://wa.me/qr/BH5CC4MSBRJBI1"
+  linkWhatsApp() {
+    window.open("https://wa.me/qr/BH5CC4MSBRJBI1", "_blank", "noopener,noreferrer");
   }
 
   downloadPDF() {
-    const link = document.createElement('a');
-    link.href = 'assets/pdf/cvPauloRicardoChagas.pdf';
-    link.download = 'cvPauloRicardoChagas.pdf';
-    link.click();
-  }
+    this.isLoading = true;
 
+    setTimeout(() => {
+      this.isLoading = false;
+
+      const link = document.createElement('a');
+      link.href = 'assets/pdf/cvPauloRicardoChagas.pdf';
+      link.download = 'cvPauloRicardoChagas.pdf';
+
+      document.body.appendChild(link);
+      link.click();
+
+      document.body.removeChild(link);
+    }, 2000);
+  }
 }
--- src/app/modules/portifolio/pages/menu/menu.component.ts
+++ src/app/modules/portifolio/pages/menu/menu.component.ts
@@ -39,7 +39,7 @@ export class MenuComponent {
       description : '"Ultimamente, tenho mergulhado em aprender e aprimorar meus conhecimentos..."'
     },
     edu : {
-      title : 'Educação',
+      title : 'Cursos',
       description : '"Ao longo dos meus estudos e experiencias no mercado desenvolvi habilidades como..."'
     },
     projects : {
@@ -206,6 +206,56 @@ export class MenuComponent {
     }
   ]
 
+  //ABOUTME
+  aboutMe = {
+    textPresentation : "Me chamo Paulo Ricardo Chagas, já estou há mais de um ano aprendendo e " +
+      "consolidando meu conhecimento em desenvolvimento de sistemas usando a linguagem Java e" +
+      " frameworks como ReactJS e Angular. Me identifico com todas as fases de codificação" +
+      " durante a confecção de um software, acho extremamente fascinante atuar desde o back " +
+      "até o front-end de um projeto, me destaco atualmente pela facilidade em me adaptar" +
+      " a projetos em andamento e por atuar na padronização de layout de sistemas.",
+
+    textPresentationEmphasis : "Tenho facilidade em trabalhar em equipe, e em todas as equipes que atuei me senti" +
+      " profundamente abraçado e compreendido por meus colegas de trabalho.",
+
+    textMyPhilosophy : "Sou um profissional que valoriza e sempre tento desenvolver minhas soft skills junto com as" +
+      " minhas hard skills. Acredito firmemente que todo projeto de software carrega uma parcela " +
+      "da história de quem o programa, por isso sempre me dedico em minhas entregas em cada linha de código por mim escrito."
+  }
+
+  toHelpYou = [
+    {
+      title : "Desenvolvimento Full-Stack",
+      informations : "Com mais de um ano de experiência em desenvolvimento de sistemas, tanto no back-end quanto" +
+        " no front-end, posso oferecer uma abordagem completa e integrada para criar soluções de software." +
+        " Utilizando a linguagem Java para o back-end e frameworks modernos como ReactJS e Angular para o front-end," +
+        " desenvolvo aplicações robustas e responsivas que atendem às necessidades específicas do seu negócio."
+    },
+    {
+      title : "Adaptação a projetos andamento",
+      informations : "Tenho uma habilidade comprovada em me adaptar rapidamente a projetos já em andamento. " +
+        "Isso significa que posso me integrar de forma eficiente à sua equipe, entender o contexto atual " +
+        "do projeto e contribuir para o progresso sem perder tempo com longas fases de adaptação. " +
+        "Minha capacidade de rapidamente compreender e seguir as práticas e padrões existentes " +
+        "garante uma continuidade suave no desenvolvimento do projeto."
+    },
+    {
+      title : "Padronização de Layouts",
+      informations : "Um dos meus pontos fortes é a padronização de layouts, garantindo que seus sistemas " +
+        "tenham uma aparência consistente e profissional. " +
+        "Isso melhora a experiência do usuário e facilita a manutenção futura."
+    },
+    {
+      title : "Desenvolvimentos de sites institucionais e pessoais",
+      informations : "Ofereço desenvolvimento de sites institucionais e pessoais com design responsivo," +
+        " identidade visual coerente, SEO otimizado, segurança e fácil navegação. Para sites pessoais, " +
+        "crio portfólios personalizados, blogs integrados e funcionalidades interativas." +
+        " Meu processo envolve consulta inicial, planejamento, desenvolvimento," +
+        " lançamento e suporte contínuo. Sou um desenvolvedor comprometido com a qualidade e satisfação do cliente." +
+        " Vamos conversar sobre como posso destacar sua presença online."
+    },
+  ]
+
   // HABILIDADES
   personalNoteHab = "Ultimamente, tenho mergulhado profundamente no aprendizado" +
     " e no aperfeiçoamento dos meus conhecimentos em desenvolvimento de sistemas. " +
@@ -248,6 +298,7 @@ export class MenuComponent {
 
   // EDU
   courseName = {
+    instituicao: "Universidade Federal Rural da Amazônia - (UFRA)",
     name: "Licenciatura em Computação",
     status: "Cursando",
     forecast: "Dezembro de 2024"

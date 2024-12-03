0253b9f614c7f42dde898b260fa6132704aea9d9
PauloRicardoDev <pauloricardochagas.dev@gmail.com>
2024-12-03 17:08:56 -0300

    ajuste no item de sobre mim

--- src/app/modules/portifolio/pages/experience/experience.component.ts
+++ src/app/modules/portifolio/pages/experience/experience.component.ts
@@ -9,4 +9,37 @@ import { Component } from '@angular/core';
 })
 export class ExperienceComponent {
 
+
+// EXPERIÊNCIA
+  experience = {
+    company: "SIS Innove e Tech",
+    duration: "1 - ano e 10 - meses",
+    office: "Estágiario Full Stack",
+    activities : "Atuar em todas as stacks na produção de um software, desde a prototipação até o deploy",
+    projects : [
+      {
+        project : 'RH - tech hunting',
+        obs: 'Sistema interno'
+      },
+      {
+        project : 'RH - Programa de Formação',
+        obs: 'Sistema interno'
+      },
+      {
+        project : 'Gestão Odonto Prev',
+        obs: 'Sistema externo'
+      },
+      {
+        project : 'RHBS - Gestão',
+        obs: 'Sistema externo'
+      }
+    ]
+  }
+
+  lastExperience = {
+    company: "Universidade Federal Rural da  Amazônia",
+    duration: "6 meses",
+    office: "Monitor Acadêmico",
+    activities : "Auxiliar em sala de aula o docente a organizar atividade e sanar dúvidas dos alunos",
+  }
 }

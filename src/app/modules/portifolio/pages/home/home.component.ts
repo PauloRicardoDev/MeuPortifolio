5204df5bcd31bc295c6b917c16d94fd1fd82bc6a
PauloRicardoDev <pauloricardochagas.dev@gmail.com>
2024-12-03 03:07:18 -0300

    ajuste do item sobre mim

--- src/app/modules/portifolio/pages/home/home.component.ts
+++ src/app/modules/portifolio/pages/home/home.component.ts
@@ -2,8 +2,8 @@ import { Component } from '@angular/core';
 import {FooterComponent} from "../../components/footer/footer.component";
 import {PresentationComponent} from "../presentation/presentation.component";
 import {MenuComponent} from "../menu/menu.component";
-import {MenuLateralComponent} from "../../components/menu-lateral/menu-lateral.component";
 import {AboutMeComponent} from "../about-me/about-me.component";
+import {Router} from "@angular/router";
 
 @Component({
   selector: 'app-home',
@@ -12,12 +12,49 @@ import {AboutMeComponent} from "../about-me/about-me.component";
     FooterComponent,
     PresentationComponent,
     MenuComponent,
-    MenuLateralComponent,
     AboutMeComponent,
   ],
   templateUrl: './home.component.html',
   styleUrl: './home.component.scss'
 })
 export class HomeComponent {
+  isLoading: boolean = false;
+
+  constructor(
+    private router: Router
+  ) {
+  }
+
+  linkLinkedin() {
+    window.open("https://www.linkedin.com/in/paulo-ricardo-chagas-99904b244?utm_source=share&utm_campaign=share_via&utm_content=profile&utm_medium=android_app", "_blank", "noopener,noreferrer");
+  }
+
+  linkWhatsApp() {
+    window.open("https://wa.me/qr/BH5CC4MSBRJBI1", "_blank", "noopener,noreferrer");
+  }
+
+  downloadPDF() {
+    this.isLoading = true;
+
+    setTimeout(() => {
+      this.isLoading = false;
+
+      const link = document.createElement('a');
+      link.href = 'assets/pdf/cvPauloRicardoChagas.pdf';
+      link.download = 'cvPauloRicardoChagas.pdf';
+
+      document.body.appendChild(link);
+      link.click();
+
+      document.body.removeChild(link);
+    }, 2000);
+  }
+
+  scrollToSection(sectionId: string): void {
+    const element = document.getElementById(sectionId);
+    if (element) {
+      element.scrollIntoView({ behavior: 'smooth', block: 'start' });
+    }
+  }
 
 }
